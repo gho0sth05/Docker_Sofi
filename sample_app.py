ae509ccdcc7aad56f387c1103c3526972c545eaf
gho0sth05 <bolnataly05@gmail.com>
2026-08-21 14:20:16 -0500

    Sube archivos D

---
+++ sample_app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+
+sample = Flask(__name__)
+
+@sample.route ("/")
+
+def main():
+    return "Me estás llamando desde " + request.remote_addr + "\n"
+
+if __name__ == "__main__":
+     sample.run (host="0.0.0.0", port=5050)
