3eea88f5ad89c7c2892e9780d001585f03a6aef6
gho0sth05 <bolnataly05@gmail.com>
2026-08-26 00:08:04 -0500

    feat: introducir los 1 fallo de seguridad  Trivy )

--- sample_app.py
+++ sample_app.py
@@ -4,28 +4,23 @@ import pymysql
 
 sample = Flask(__name__)
 
-
 @sample.route("/")
 def home():
-  try:
-    conn = pymysql.connect(
-        host="servidor-bd",
-        user="root",
-        password=os.getenv("DB_PASSWORD"),
-        database="082_db",
-        MYSQL_PASSWORD = "super_secret_123"
-    )
-    conn.close()
-    db_status = (
-        "Conexión exitosa a la base de datos, prueba para CI/CD para"
-        " despliegue continuo"
-    )
-  except Exception as e:
-    db_status = f"Error al conectar a la base de datos: {e}"
+    try:
+        conn = pymysql.connect(
+            host="servidor-bd",
+            user="root",
+            password=os.getenv("DB_PASSWORD"), # SOLUCIÓN: Variable de entorno
+            database="082_db"
+        )
+        conn.close()
+        db_status = "Conexión exitosa a la base de datos, prueba para CI/CD"
+    except Exception as e:
+        db_status = f"Error al conectar a la base de datos: {e}"
 
-  return f"<h1>Bienvenido a mi aplicación Sofia</h1><p>{db_status}</p>"
-
+    return f"<h1>Bienvenido a mi aplicación Sofia</h1><p>{db_status}</p>"
 
 if __name__ == "__main__":
-  modo_debug = os.getenv("FLASK_DEBUG", "True").lower() == "true"
-  sample.run(host="0.0.0.0", port=5051, debug=True)  # nosec B104
+    # SOLUCIÓN: Controlado por variable de entorno, apagado por defecto
+    modo_debug = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+    sample.run(host="0.0.0.0", port=5051, debug=modo_debug) # nosec B104
